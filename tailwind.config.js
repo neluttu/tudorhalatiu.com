6967d0cd0d545e41bae8b6e525700ad815feea30
Neluttu <neluttu@gmail.com>
2023-07-25 15:31:26 +0300

    Added some files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html','styles.css'],
+  theme: {
+    extend: {
+        fontFamily: {
+            'nunito': ['Nunito', 'sans-serif'],
+            'rubik': ['Rubik', 'sans-serif'],
+            'poppins': ['Poppins', 'sans-serif'],
+        },
+        backgroundImage: {
+            'tudor': "url('/images/tudor_halatiu.jpg')",
+        },
+    },
+  },
+  plugins: [],
+}
