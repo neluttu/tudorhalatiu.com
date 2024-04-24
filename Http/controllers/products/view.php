7de22208cc1dcc4a8fae71274b0c5b92d0ffd260
Neluttu <neluttu@gmail.com>
2024-04-25 01:10:56 +0300

    Fixed views

--- Http/controllers/products/view.php
+++ Http/controllers/products/view.php
@@ -7,12 +7,13 @@ $db = App::resolve('Core\Database');
 $productViewCounter = new ProductViewCounter();
 $productViewCounter->incrementProductView($params['id']); 
 
-$product = $db->query('SELECT id, products.name, category, sizes, price, excerpt, description, status, categories.name AS category_name, views FROM products LEFT JOIN categories ON categories.category_id = products.category LEFT JOIN product_views ON product_views.product_id = products.id WHERE id = :id AND status = "Online"', 
+$product = $db->query('SELECT id, products.name, category, sizes, price, excerpt, description, status, categories.name AS category_name FROM products LEFT JOIN categories ON categories.category_id = products.category WHERE id = :id AND status = "Online"', 
                     [
                         'id' => $params['id']
                     ])->findOrFail();
 
 $categories = $db->query('SELECT category_id, name, slug FROM categories')->get();
+$getViews = $db->query('SELECT count(views) AS views FROM product_views WHERE product_id = :id', [':id' => $params['id']])->get();
 
 $imagesFiles = glob(base_path('public/images/products/'.$product['id'].'/*.{jpg,png,avif,jpeg}'), GLOB_BRACE);
 if($imagesFiles !== false and count($imagesFiles) > 0)
@@ -29,6 +30,6 @@ view('products/view', [
     'title' => $product['name'] . ' - Tudor Halațiu',
     'description' => $product['excerpt'] . ' - Tudor Halațiu',
     'categories' => $categories,
-    'views' => $product['views'],
+    'views' => $getViews[0]['views'],
     'photos' => $imagesFiles
 ]);
